0681b1b05c7b634dba26cf7023cf661e6a05b050
shaded-enmity <the.arul@gmail.com>
2015-06-25 20:01:17 +0200

    run: preliminary support for argparse

--- run.py
+++ run.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python -tt
 
-import sys, subprocess, tempfile
+import sys, subprocess, tempfile, argparse, json, os
 
 def usage():
 	print(os.path.basename(sys.argv[0]) + ' SOURCE TARGET\n')
-        print ' Scrape all image manifest files from SOURCE directory'
-        print ' and copy them to TARGET. The files are then collated '
-        print ' into a repository-wide repodata.json'
-        sys.exit(1)
+	print ' Scrape all image manifest files from SOURCE directory'
+	print ' and copy them to TARGET. The files are then collated '
+	print ' into a repository-wide repodata.json'
+	sys.exit(1)
+
+ap = argparse.ArgumentParser()
+ap.add_argument('--confdir', '-c')
+ap.add_argument('SOURCE')
+ap.add_argument('TARGET')
+
+config = None
+confdir = 'conf/'
+args = ap.parse_args()
+if hasattr(args, 'confdir'):
+	confdir = args.confdir
+source = args.SOURCE
+repodatadir = args.TARGET
+
+cp = os.path.join(confdir, 'ims.json')
+with open(cp, 'r') as f:
+	config = json.load(f)
 
 # Add parameters:
 #
@@ -25,7 +42,6 @@ if len(sys.argv) != 3:
 # 4. sign.py    REPODATADIR [FINGERPRINT]
 ###
 
-source, repodatadir = sys.argv[1:3]
 tmpcache = tempfile.mkdtemp()
 
 scrape = subprocess.check_call(['./scrape.py', source, tmpcache])
